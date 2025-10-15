df02a3ed00f5eb1120f20ab5a37c6c4d45baa0a2
Malakusta <vlad.adragai@gmail.com>
2025-10-15 14:58:50 +0300

    Am facut dictionare

---
+++ py/dictionaries.py
@@ -0,0 +1,6 @@
+#Add a list named house with the houses form Harry Potter
+houses = {"Harry": "Gryffindor", "Ron": "Gryffindor", "Hermione": "Gryffindor", "Draco": "Slytherin"}
+
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
