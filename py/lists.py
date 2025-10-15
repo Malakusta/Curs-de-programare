5927ccb592c1677b193bc32daca716a05ec3367f
Malakusta <vlad.adragai@gmail.com>
2025-10-15 14:55:16 +0300

    Lesson2

---
+++ py/lists.py
@@ -0,0 +1,6 @@
+#Define a list of names
+names = ["Alice", "Bob", "Charlie", "Diana"]
+print(names[0])
+
+names.sort()
+print(names)
---
+++ py/sets.py
@@ -0,0 +1,14 @@
+#Create an empty set
+my_set = set()
+
+#Add elements to the set
+
+my_set.add(1)
+my_set.add(2)
+my_set.add(3)
+my_set.add(4)
+
+my_set.remove(2)
+
+print(my_set)
+print(f"There are {len(my_set)} elements in the set.")
